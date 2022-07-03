06bb4e3c347e40eccf062e358aa72e914aa7fadf
mustafapir <mustafapir29@gmail.com>
2022-07-04 02:08:57 +0300

    Update drawProt.R

--- R/drawProt.R
+++ R/drawProt.R
@@ -23,7 +23,7 @@ drawProt<-function(table, protein_id, organisms){
     colors<-c("#FF1F5B","#009ADE")
     table<-table[table[[paste0(organisms[i], "_ID")]] == protein_id[i],]
     SNP <- table[[paste0(organisms[i], "_aapos")]]
-    gene1 <- GRanges("chr1", IRanges(SNP, width=1, names=paste0(table[[paste0(organisms[i], "_from")]],
+    gene1 <- GenomicRanges::GRanges("chr1", IRanges(SNP, width=1, names=paste0(table[[paste0(organisms[i], "_from")]],
                                                                 table[[paste0(organisms[i], "_aapos")]],
                                                                 table[[paste0(organisms[i], "_to")]])))
     gene1$border <- sample(c("gray30"), length(SNP), replace=TRUE)
@@ -32,7 +32,7 @@ drawProt<-function(table, protein_id, organisms){
     gene1.rot$label.parameter.rot <- 45
     gene1.rot$SNPsideID<-"top"
 
-    features1 <- GRanges("chr1", IRanges(start=c(1, info$tbl$pfam_start, info$len),
+    features1 <- GenomicRanges::GRanges("chr1", IRanges(start=c(1, info$tbl$pfam_start, info$len),
                                          width=c(0, info$tbl$pfam_end-info$tbl$pfam_start, 0),
                                          names=c("", info$dom, "")))
     features1$fill <- c("#FF1F5B", colors[1:length(info$tbl$pfam_start)], "#009ADE")
