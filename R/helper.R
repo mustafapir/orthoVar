004ca6d0c87384bb87c46a01978f5c5ef3056f2c
Mustafa Samet Pir <mustafapir29@gmail.com>
2025-08-30 20:20:27 +0300

    Add orthology parameter to ortho_convert function

--- R/helper.R
+++ R/helper.R
@@ -140,7 +140,7 @@ getlinks<-function(species1, species, annot){
 
 }
 
-ortho_convert<-function(species1, species, ...){
+ortho_convert<-function(species1, species, orthology, ...){
 
   # filter data based on user input
   orr<-orthology %>%
